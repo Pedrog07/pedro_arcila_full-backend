3bdfb4355bc4a7adb42a3adfc07c3ba952222c63
Pedro Arcila <pedro_210595@hotmail.com>
2022-07-07 08:00:31 -0400

    feat: complete login endpoint

--- src/endpoints/auth/auth.provider.ts
+++ src/endpoints/auth/auth.provider.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'entities';
+import * as bcrypt from 'bcrypt';
+import { LoginDTO } from './types';
+import { ExceptionsProvider } from 'providers';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthProvider {}
+export class AuthProvider {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private readonly exceptionsProvider: ExceptionsProvider,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(data: LoginDTO) {
+    const { email, password } = data;
+
+    if (!email || !password) {
+      this.exceptionsProvider.throwCustomException(
+        this.exceptionsProvider.httpStatus.BAD_REQUEST,
+        'Missing some fields',
+      );
+    }
+
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      this.exceptionsProvider.throwCustomException(
+        this.exceptionsProvider.httpStatus.UNAUTHORIZED,
+        'Does not have access',
+      );
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      this.exceptionsProvider.throwCustomException(
+        this.exceptionsProvider.httpStatus.UNAUTHORIZED,
+        'Invalid password',
+      );
+    }
+
+    const accessToken = this.jwtService.sign(
+      { sub: user.id },
+      {
+        secret: process.env.JWT_SECRET,
+      },
+    );
+
+    return {
+      accessToken,
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    };
+  }
+}
--- src/providers/authorization.provider.ts
+++ src/providers/authorization.provider.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthorizationProvider {
-  test() {
-    return 'authorized';
+  constructor(private readonly jwtService: JwtService) {}
+  validateToken(token: string) {
+    const { sub } = this.jwtService.verify(token, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    if (!sub) {
+      throw new HttpException(
+        { statusCode: HttpStatus.FORBIDDEN, message: 'Invalid token' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    return sub;
   }
 }
